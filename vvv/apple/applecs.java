65e388e40b9129f043733b533033452605777d12
wusixing <1079289194@qq.com>
2019-06-24 21:14:39 +0800

    颜色和重量

--- vvv/apple/applecs.java
+++ vvv/apple/applecs.java
@@ -1,5 +1,21 @@
 package apple;
 
 public class applecs {
+    private String color;
+    private double weight;
 
+    public void apple(double weight, String color) {
+        this.color = color;
+        this.weight = weight;
+    }
+    
+    
+    public String getColor() {
+        return color;
+    }
+    
+    
+    public void setColor() {
+
+    }
 }
