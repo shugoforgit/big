b0f34cb8be362c276ee4702613f8216d4b22277a
wusixing <1079289194@qq.com>
2019-06-25 19:32:09 +0800

    apple

--- vvv/apple/apple.java
+++ vvv/apple/apple.java
@@ -5,20 +5,32 @@ public class apple {
     private String color;
     private double weight;
 
-    public apple(double weight, String color) {
+    public apple(String color, double weight) {
         this.color = color;
         this.weight = weight;
     }
-    
-    
+
     public String getColor() {
         return color;
     }
-    
-    
-    public void setColor() {
 
+    public void setColor(String color) {
+        this.color = color;
     }
-  
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String toString() {
+        return "Apple{" +
+                "color='" + color + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
 }
 
